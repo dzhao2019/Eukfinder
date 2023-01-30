6ec44467226a8da6f2becf0f92a142c6ad1b0ba8
dzhao2019 <39600837+dzhao2019@users.noreply.github.com>
2023-01-30 12:28:42 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="eukfindertest",
-    version="1.2.3",
-    author="dzhao,dsalas",
-    author_email="dandan.tanny.zhao@email.com, dayana.salas.leiva@gmail.com",
+    name="eukfindertest2",
+    version="1.0.1",
+    author="dzhao",
+    author_email="dandan.tanny.zhao@email.com",
     description="A small demo package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/dzhao2019/Eukfinder",
+    url="https://github.com/dzhao2019/eukfindertest",
     packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
