97934c6c3c3eeedb4143e2051852902753896bf2
dzhao2019 <ilyzdd@gmail.com>
2024-12-10 09:19:48 -0400

    update

--- setup.py
+++ setup.py
@@ -1,21 +1,26 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="eukfindertest2",
-    version="1.0.1",
+setup(
+    name="eukfinder",
+    version="1.2.3",
     author="dzhao",
     author_email="dandan.tanny.zhao@email.com",
-    description="A small demo package",
+    description="",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/dzhao2019/eukfindertest",
-    packages=setuptools.find_packages(),
-    classifiers=(
+    packages=find_packages(),
+    classifiers=([
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ),
+        "Operating System :: OS Independent"
+    ]),
+    scripts=["bin/Eukfinder.py"],
+    entry_points={
+        'console_scripts': [
+            'eukfinder = Eukfinder:main',
+        ],
+    }
 )
