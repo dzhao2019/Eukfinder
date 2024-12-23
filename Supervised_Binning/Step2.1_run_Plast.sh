abba1d58775e2f334e8ce5ad5a1c714faf3e16a6
dzhao2019 <ilyzdd@gmail.com>
2024-12-23 15:16:45 -0400

    Update Step2.1_run_Plast.sh

--- Supervised_Binning/Step2.1_run_Plast.sh
+++ Supervised_Binning/Step2.1_run_Plast.sh
@@ -7,7 +7,7 @@ cd $PWD
 source activate eukfinder
 query=Eukfinder_long.fasta
 # Run plast
-DB2=/scratch5/db/Eukfinder/nt2021/nt.fasta
-plast -e 1E-5 -max-hit-per-query 1 -outfmt 1 -a 48 -p plastn -i $query -d $DB2 -force-query-order 1000 -o ${query::-6}.PLAST_nt.tsv
+DB=/scratch5/db/Eukfinder/nt2021/nt.fasta
+plast -e 1E-5 -max-hit-per-query 1 -outfmt 1 -a 48 -p plastn -i $query -d $DB -force-query-order 1000 -o ${query::-6}.PLAST_nt.tsv
 
 conda deactivate
