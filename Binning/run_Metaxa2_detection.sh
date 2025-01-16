6936c03b0bea0a315e334bde86fe0180cffe0f52
dzhao2019 <ilyzdd@gmail.com>
2025-01-16 10:33:26 -0400

    remove log lines

--- Binning/run_Metaxa2_detection.sh
+++ Binning/run_Metaxa2_detection.sh
@@ -7,19 +7,11 @@ conda activate metaxa2_env  # Replace with the name of your Conda environment
 # Create a directory for Metaxa2 results
 mkdir -p Metaxa2_results
 
-# Log the start of the process
-echo "==ERR321632_Eukfinder_long.fasta begins=="
-
 # Run Metaxa2 for SSU
 metaxa2 --cpu 20 -g SSU -i Eukfinder_long.fasta -o Eukfinder_long_metaxa2_SSU
-date
 
 # Run Metaxa2 for LSU
 metaxa2 --cpu 20 -g LSU -i Eukfinder_long.fasta -o Eukfinder_long_metaxa2_LSU
-date
-
-# Log the completion of the process
-echo "==ERR321632_Eukfinder_long.fasta finished=="
 
 # Delete empty files
 find . -type f -size 0 -delete
