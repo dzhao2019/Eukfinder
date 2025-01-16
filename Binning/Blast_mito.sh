33ed5d9cfffa407e504d54339cf112a9b0986352
dzhao2019 <ilyzdd@gmail.com>
2025-01-16 10:35:51 -0400

    rewritten script to make it portable for all systems

--- Binning/Blast_mito.sh
+++ Binning/Blast_mito.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
-#$ -S /bin/bash
-. /etc/profile
-#$ -cwd
-#$ -pe threaded 30
-cd $PWD
 
+# Activate Conda environment for BLAST
+source $(conda info --base)/etc/profile.d/conda.sh
+conda activate blast
+
+# Define input query file
 query=Eukfinder_long.fasta
 
-source activate blast
-export BLASTDB=/scratch5/db/Eukfinder/Mitochondrial
-DB=mito_blast_db
-blastn -db $DB -query $query -out ${query::-6}_BLAST4Mit.out -num_threads 30 -outfmt "6 qseqid sseqid stitle evalue pident qcovhsp nident mismatch length slen qlen qstart qend sstart send staxids sscinames sskingdoms" -evalue 1E-5 -max_hsps 1
-conda deactivate
+# Define the BLAST database path and name
+# Users must modify the `DB` variable to point to their local BLAST database.
+DB=/path/to/your/blast_database/mito_blast_db
 
+# Check if the database exists
+if [ ! -f "${DB}.nin" ]; then
+  echo "Error: BLAST database '${DB}' not found. Please specify the correct path."
+  exit 1
+fi
+
+# Run BLAST
+blastn -db $DB \
+       -query $query \
+       -out ${query::-6}_BLAST4Mit.out \
+       -num_threads 30 \
+       -outfmt "6 qseqid sseqid stitle evalue pident qcovhsp nident mismatch length slen qlen qstart qend sstart send staxids sscinames sskingdoms" \
+       -evalue 1E-5 \
+       -max_hsps 1
+
+# Deactivate Conda environment
+conda deactivate
